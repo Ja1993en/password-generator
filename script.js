bf709134185b8c6f66ed0f0fc86353c6aac61028
Basquiat <jlnmcneal@gmail.com>
2022-09-25 11:47:17 -0500

    Boolean statement output

--- script.js
+++ script.js
@@ -1,61 +1,90 @@
-//Assignment Code
+//Assignment Codeas
 var generateBtn = document.querySelector("#generate");
 
-var allowUpperCase;
-var allowLowerCase;
+var allowUppercase;
+var allowLowercase;
 var allowNumbers;
 var allowSpecials;
-var interger;
+var passwordLength;
 var passwordCharacterCount;
+var myPasscode;
+var passwords = [];
+var uppercaseChars = ["ABCDEFGHIJKLMNOP"];
+var lowercaseChars = ["abcdefghijklmnop"];
+var numbers = [123456789]
+var specialChars = ["!@#$%^&*"];
+var randomStr;
+var finalArr;
 
-var uppercaseChars = ["A", "B","C","D","E","F","G","H","I","J","K","L","L","M"];
-var lowercaseChars = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n"];
-var numbers = [1,2,3,4,5,6,7,8,9,]
-var allowSpecials = ["!","@","#","$","%","^"];
+function randomNumberGenerator(min,max){
+    return Math.floor(Math.random() * (max - min + 1) ) + min;
+}
 
-
-
-
-
+// This prompt statments are basically boolean statements(true or false)
+// Based on true or false the boolean will apend the array the the empty variable.
+// Ex. (passwords += upperChars);
 function askAboutUppercase(){
     allowUppercase = confirm("Are uppercase characters allowed");
-    console.log(allowUppercase)
+    if (allowUppercase === true ){
+    passwords += uppercaseChars;
     }
+    }
+    
     
     function askAboutLowerCase(){
     allowLowercase = confirm("Are lowercase characters allowed");
-    console.log(allowLowercase)
+    if(allowLowercase === true){
+        passwords += lowercaseChars;
     }
+}
     
     function askAboutNumbers(){
     allowNumbers = confirm("Are numbers chracters allowed ");
-    var interger = parseInt(allowNumbers);
-    // var interger = parseInt(allowNumbers);
-    console.log(interger)
-    
+    if(allowNumbers === true){
+        passwords += numbers;
     }
-    
-    function howManyCharacters(){
-    var passwordCharacterCount = ("How many characters would you like in your password")
     }
-    
+
     function askAboutSpecials(){
-    allowSpecials = confirm("Are specials chracters allowed");
-    console.log(allowSpecials);
+       allowSpecials = confirm("Are special characters allowed");
+        if(allowSpecials === true){
+            passwords += specialChars;
+          
+        }
+        }
+           
+    function howManyCharacters(){
+    var passwordCharacterCount = prompt("How many characters would you like in your password");
+    var passwordLength = parseInt(passwordCharacterCount);
+    console.log(passwordLength);
+    return passwordLength
     }
-    
-    
+   
+   
     function generatePassword(){
-     
+     var finalArr = [];
+     var randomStr = "";
+   
+     for ( var i = 1; i <= 10; i++ ){
+     var randomIdx = randomNumberGenerator(1, passwords.length -1);
+   var myPasscode = passwords[randomIdx];
+    randomStr += myPasscode;
+    finalArr.push(randomStr);
+    console.log(finalArr);
+     }
+     return finalArr
     }
+    
     
     // Write password to the #password input
-    function writePassword() {
+    function writePassword(){  
+     howManyCharacters();
       askAboutLowerCase();
       askAboutUppercase();
-      askAboutNumbers();
       askAboutSpecials();
-    
+      askAboutNumbers();
+
+      console.log(passwords);
       var password = generatePassword();
       var passwordText = document.querySelector("#password");
     
@@ -66,4 +95,11 @@ function askAboutUppercase(){
     // Add event listener to generate button
     generateBtn.addEventListener("click", writePassword);
     
-  
+
+
+//     var uppercaseChars = ["A", "B","C","D","E","F","G","H","I","J","K","L","L","M"];
+// var lowercaseChars = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n"];
+// var numbers = [1,2,3,4,5,6,7,8,9,]
+// var specialChars = ["!","@","#","$","%","^"];
+// var randomStr;
+// var finalArr;
